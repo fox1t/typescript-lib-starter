7937fc44bc8301145614428553fc4f21bd1f5098
Huafu Gandon <huafu.gandon@gmail.com>
2018-10-09 15:50:56 +0200

    fixes tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  roots: ['<rootDir>'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'ts-jest'
 }
