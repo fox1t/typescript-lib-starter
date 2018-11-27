f29e7e7098bb493a66b846d02fd03723c8121119
Maksim Sinik <maksim@sinik.it>
2018-11-27 11:04:22 +0100

    Removes jest
    Fixes event-stream vulnerability

--- __tests__/index.ts
+++
@@ -1,5 +0,0 @@
-import code from '../src'
-
-test('test default function code', () => {
-  expect(code()).toBe('put your source code here')
-})
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'tsconfig.test.json',
-    },
-  },
-}
